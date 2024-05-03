8885d60832eb3ff1d67d9a4132d67faf62a4ccb7
Mare Ettiene <Mare.Ettiene@bakerhughes.com>
2024-05-03 11:49:35 +0200

    Add grpc

--- DiviGrpcService/DiviGrpcService/Program.cs
+++ DiviGrpcService/DiviGrpcService/Program.cs
@@ -1,6 +1,8 @@
 using DiviGrpcService.Services;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<DeviceDataService>();
 
 // Add services to the container.
 builder.Services.AddGrpc();
@@ -9,6 +11,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DeviceDataService>();
+app.MapGet("/Data", async ([FromServices] DeviceDataService service) => await service.GetData());
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
---
+++ DiviGrpcService/DiviGrpcService/Services/DeviceDataService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using Google.Protobuf;
+
+namespace DiviGrpcService.Services;
+
+public class DeviceDataService : DataService.DataServiceBase
+{
+    private readonly ILogger<GreeterService> _logger;
+
+    public DeviceDataService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<DataReply> GetData(DataRequest request, ServerCallContext context) => GetData();
+
+    public Task<DataReply> GetData()
+    {
+        var dataJson = File.ReadAllText("Data\\NodeId_8-FileId.json");
+        
+        var message = Activator.CreateInstance(typeof(DataReply)) as IMessage;
+        var result = (DataReply)JsonParser.Default.Parse(dataJson, message?.Descriptor);
+        return Task.FromResult(result);
+    }
+}
